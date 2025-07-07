0606d752498f7c54b6ecc277f2ea156e935b22a0
ian <ianjudo552@gmail.com>
2025-07-07 15:45:33 -0300

    insert user

--- src/main/java/com/ian/mongospring/services/UserService.java
+++ src/main/java/com/ian/mongospring/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ian.mongospring.services;
 
 import com.ian.mongospring.domain.User;
+import com.ian.mongospring.dto.UserDTO;
 import com.ian.mongospring.repository.UserRepository;
 import com.ian.mongospring.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,15 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+            return repo.insert(obj);
+    }
+    public User fromDTO(UserDTO objDto){
+            return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
+
     }
 
 }
